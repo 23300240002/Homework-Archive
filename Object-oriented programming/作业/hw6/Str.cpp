cb3b06254963bfffc1493dd7ee2d365fafbc1a63
Yifeng Guo <23300240002@m.fudan.edu.cn>
2026-06-01 16:26:51 +0800

    Add files via upload

---
+++ Object-oriented programming/作业/hw6/Str.cpp
@@ -0,0 +1,43 @@
+#include "Str.h"
+
+Str& Str::operator+=(const Str& s)
+{
+	std::copy(s.data.begin(), s.data.end(), std::back_inserter(data));
+	return *this;
+}
+
+std::istream& operator>>(std::istream& is, Str& s)
+{
+	s.data.clear();
+
+	char c;
+	while (is.get(c) && std::isspace(c))
+		;
+
+	if (is) {
+		do {
+			s.data.push_back(c);
+		} while (is.get(c) && !std::isspace(c));
+
+		if (is) {
+			is.unget();
+		}
+	}
+
+	return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const Str& s)
+{
+	for (Str::size_type i = 0; i != s.size(); ++i) {
+		os << s[i];
+	}
+	return os;
+}
+
+Str operator+(const Str& s, const Str& t)
+{
+	Str r = s;
+	r += t;
+	return r;
+}
---
+++ Object-oriented programming/作业/hw6/Str.h
@@ -0,0 +1,43 @@
+#ifndef _STRING_H
+#define _STRING_H
+
+#include <algorithm>
+#include <cstddef>
+#include <iostream>
+#include <cstring>
+#include "Vec.h"
+
+class Str{
+	friend std::istream& operator>>(std::istream&, Str&);
+
+public:
+	typedef Vec<char>::size_type size_type;
+
+	Str() { }
+	Str(size_type n, char c) : data(n, c) { }
+	Str(const char* cp) {
+		std::copy(cp, cp + std::strlen(cp), std::back_inserter(data));
+	}
+
+	template <class In>
+	Str(In b, In e) {
+		std::copy(b, e, std::back_inserter(data));
+	}
+
+	size_type size() const { return data.size(); }
+
+	char& operator[](size_type i) { return data[i]; }
+	const char& operator[](size_type i) const { return data[i]; }
+
+	Str& operator+=(const Str&);
+
+private:
+	Vec<char> data;
+};
+
+std::istream& operator>>(std::istream&, Str&);
+std::ostream& operator<<(std::ostream&, const Str&);
+Str operator+(const Str&, const Str&);
+
+
+#endif
